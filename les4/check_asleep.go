9e37f41300af8e4ad970f5df8b59fe6b7f01fe97
GurovSD <gurov.sd@gmail.com>
2021-08-19 19:37:46 +0300

    check asleep 2

--- les4/check_asleep.go
+++ les4/check_asleep.go
@@ -5,40 +5,18 @@ import "fmt"
 func main() {
 
 	ch_counter := make(chan int)
-	// var workers = make(chan struct{}, 1000)
-
-	// value := 0
+	ch_res := make(chan int)
 
 	ch_counter <- 0
-	value := <-ch_counter
 
-	fmt.Println(value)
+	go func() {
 
-	// for i := 0; i < 1000; i++ {
-	// 	workers <- struct{}{}
+		val := <-ch_counter
+		ch_res <- val
 
-	// go func() {
-	// 	// defer func() {
-	// 	// 	<-workers
-	// 	// }()
+	}()
 
-	// 	buf_val := <-ch_counter
-	// 	fmt.Println(buf_val)
-	// 	buf_val++
-	// 	// ch_counter <- buf_val
-
-	// }()
-
-	// }
-
-	// fmt.Println(len(workers))
-
-	// for {
-	// 	if len(workers) == 0 {
-	// 		fin_value := <-ch_counter
-	// 		fmt.Println(fin_value)
-	// 		break
-	// 	}
-	// }
+	val := <-ch_res
+	fmt.Println(val)
 
 }
